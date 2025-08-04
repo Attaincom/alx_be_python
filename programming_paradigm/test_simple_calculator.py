ed500a18d3b20a8e2f5e76a73041259be700b340
Attaincom <olatomidefagbohun@gmail.com>
2025-08-04 07:11:25 +0100

    library

---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,29 @@
+import unittest
+from simple_calculator import SimpleCalculator
+
+class TestSimpleCalculator(unittest.TestCase):
+
+    def setUp(self):
+        self.calc = SimpleCalculator()
+
+    def test_addition(self):
+        self.assertEqual(self.calc.add(2, 3), 5)
+        self.assertEqual(self.calc.add(-2, 3), 1)
+
+    def test_subtraction(self):
+        self.assertEqual(self.calc.subtract(5, 3), 2)
+        self.assertEqual(self.calc.subtract(0, 3), -3)
+
+    def test_multiplication(self):
+        self.assertEqual(self.calc.multiply(4, 2), 8)
+        self.assertEqual(self.calc.multiply(-1, 5), -5)
+
+    def test_division(self):
+        self.assertEqual(self.calc.divide(10, 2), 5.0)
+        self.assertEqual(self.calc.divide(5, 2), 2.5)
+
+    def test_division_by_zero(self):
+        self.assertIsNone(self.calc.divide(5, 0))
+
+if __name__ == "__main__":
+    unittest.main()
